8ba75ff61ab346a383b4d83504530de701148719
Shubham Lad <shubhamlad1001@gmail.com>
2021-01-29 21:52:32 +0530

    reason

---
+++ src/components/Home/Reason/Reason.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Reason = () => {
+  return (
+    <div className="container text-center p-4">
+      <p className="gb-heading font-50">Why GreenBord</p>
+      <p className="font-18">
+        An ecosystem where Institutes (School, College and Coaching) can provide
+        learning to their student beyond classrooms and also list out their
+        courses to provide distance learning to other students
+      </p>
+    </div>
+  );
+};
+
+export default Reason;
--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ImageHeading from "../../components/Home/Image/ImageHeading";
+import Reason from "../../components/Home/Reason/Reason";
 
 const HomePage = () => {
   return (
     <div>
       <ImageHeading />
+      <Reason />
     </div>
   );
 };

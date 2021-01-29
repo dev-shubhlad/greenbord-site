4610abc71f93271af700e64d938a5447331ff989
Shubham Lad <shubhamlad1001@gmail.com>
2021-01-29 18:17:57 +0530

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import linkedin from "../../assets/images/linkedin.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="gb-footer">
+      <div className="container p-4">
+        <div className="row">
+          <div className="col">
+            <div>
+              GREENBORD <br /> Patratu Ramgarh,
+              <br /> Jharkhand - 829126
+            </div>
+            <div className="pt-1">
+              <Link to="/about-us" className="link-text">
+                About Us
+              </Link>
+            </div>
+            <div className="p-2">
+              <a
+                href="https://in.linkedin.com/company/greenbord"
+                target="_blank"
+              >
+                <img src={linkedin} width="40" />
+              </a>
+            </div>
+          </div>
+          <div className="col">
+            <div>
+              Contact us <br />
+              Email ID: greenbord2020@gmail.com <br />
+              WhatsApp Chat: 7970601950
+            </div>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          © 2020 GreenBord. All Rights Reserved
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
